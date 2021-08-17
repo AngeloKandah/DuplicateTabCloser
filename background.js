a42b6657472d81b0ea9abe390f01c882337d230f
Angelo Kandah <angelokandah@gmail.com>
2021-08-16 21:26:40 -0700

    Added tab moving and changing tabs

--- background.js
+++ background.js
@@ -2,7 +2,6 @@ let tab_list = {};
 
 function hasDuplicates(tab_id, tab_url) {
     for (let tab_list_id in tab_list) {
-        console.log(tab_list_id, tab_id, tab_list[tab_list_id], tab_url)
         if (tab_list_id != tab_id && tab_list[tab_list_id] === tab_url) {
             return true
         }
@@ -11,11 +10,30 @@ function hasDuplicates(tab_id, tab_url) {
 }
 
 function removeDuplicate(tab_id) {
-    chrome.tabs.remove(tab_id)
+    chrome.tabs.remove(tab_id, function ignore_error() {void chrome.runtime.lastError;})
+}
+
+function changeTab(tab_url){
+    for (let tab_list_id in tab_list) {
+        if(tab_url === tab_list[tab_list_id]){
+            chrome.tabs.update(parseInt(tab_list_id), {active: true})
+        }
+    }
+}
+
+function moveTab(tab_url, openerTab){
+    console.log(tab_url,openerTab)
+    for (let tab_list_id in tab_list){
+        if (tab_url === tab_list[tab_list_id]){
+            console.log(chrome.tabs.get(openerTab, function(tab) {
+                chrome.tabs.move(parseInt(tab_list_id), {index: tab.index+1})
+            }))
+        }
+    }
 }
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log(tabId, tab.url, changeInfo);
+    console.log(tabId, tab.url, changeInfo, tab.openerTabId);
     if (changeInfo.url) {
         return
     }
@@ -24,8 +42,15 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         return
     }
     removeDuplicate(tabId)
+    if(!tab.openerTabId){
+        changeTab(tab.url)
+        return
+    }
+    moveTab(tab.url, tab.openerTabId)
 })
 
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    delete tab_list[tabId]
+chrome.tabs.onRemoved.addListener((tabId) => {
+    if(tab_list[tabId]){
+        delete tab_list[tabId]
+    }
 })
