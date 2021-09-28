3abbe07b2a669df5e519f16184adb8644ba96ccd
Colby Leiske <colby.leiske@gmail.com>
2021-09-27 19:29:18 -0700

    Promisify storage calls and move to single options object (#4)
    * Update options to single object and add utility functions
    
    add missing JSDoc
    
    add missing key
    
    * Update background.js
    
    * Update background.js

--- options.js
+++ options.js
@@ -1,42 +1,65 @@
-chrome.storage.local.get(
-    ["options", "exclusionArray"],
-    ({
-        options = {
-            moveTabs: true,
-            effectWindows: false,
-            effectTabGroups: false,
-        },
-        exclusionArray = [],
-    }) => {
-        const { moveTabs, effectWindows, effectTabGroups } = options;
+
+/**
+ * Read from local storage in async instead of using callbacks
+ * Pass in a single string key or an array of keys to retrieve multiple values
+ * 
+ * @param {string|array<string>} key 
+ * @returns {object}
+ */
+const getLocalStorageKey = key => {
+    let storageKey = key;
+    if (typeof storageKey !== 'array') storageKey = [storageKey];
+
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.local.get(key, resolve)
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+/**
+ * Set an object in local storage to the given value
+ * Local Stoarge objects key is based on the Object key passed in
+ * @param {Object} val 
+ */
+const setLocalStorageValue = val => chrome.storage.local.set(val);
+
+/**
+ * Upsert into options local storage namespace
+ * Local Stoarge objects key is based on the Object key passed in
+ * @param {Object} val 
+ */
+const updateOptions = async (updatedValues) => {
+    const { options } = await getLocalStorageKey('options');
+    const updatedOptions = {
+        ...options,
+        ...updatedValues,
+    }
+    setLocalStorageValue({ options: updatedOptions });
+}
+
+getLocalStorageKey('options')
+    .then(({ options }) => {
+        const { moveTabs, effectWindows, effectTabGroups, exclusions } = options;
         document.getElementById("moveTabs").checked = moveTabs;
         document.getElementById("effectWindows").checked = effectWindows;
         document.getElementById("effectTabGroups").checked = effectTabGroups;
-        createHTMLList(exclusionArray);
-        chrome.storage.local.set({
-            options: {
-                moveTabs,
-                effectWindows,
-                effectTabGroups,
-            },
-            exclusionArray,
-        });
-    }
-);
+        createHTMLList(exclusions);
+    });
+
 
 const submit = document.getElementById("submit");
 const exclusionBox = document.getElementById("exclusionArray");
 const exclusionList = document.getElementById("exclusions");
 
-function isPropertyEnabled(id) {
+function isOptionEnabled(id) {
     return document.getElementById(id).checked;
 }
 
-function createHTMLList(exclusionArray) {
-    exclusionArray.forEach((item) => {
-        addToHTMLList(item);
-
-    });
+function createHTMLList(exclusions) {
+    exclusions.forEach(addToHTMLList);
 }
 
 function addToHTMLList(item) {
@@ -52,54 +75,42 @@ function createDeleteButton() {
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     deleteButton.className = "DeleteBtn";
-    deleteButton.addEventListener("click", () => {
+    deleteButton.addEventListener("click", async () => {
         const itemToRemove = deleteButton.parentElement;
         itemToRemove.remove();
-        chrome.storage.local.get(
-            { exclusionArray: [] },
-            ({ exclusionArray: oldExclusionArray }) => {
-                const exclusionArray = oldExclusionArray.filter(
-                    (item) => item !== itemToRemove.firstChild.data
-                );
-                chrome.storage.local.set({ exclusionArray });
-            }
-        );
+        const { firstChild: { data: removedExclusion } } = itemToRemove;
+        const { options: { exclusions } } = getLocalStorageKey('options');
+        const newExclusions = exclusions.filter(item => item !== removedExclusion);
+        await updateOptions({ exclusions: newExclusions });
     });
     return deleteButton;
 }
 
-function submitChanges() {
+async function submitChanges() {
     const settingFields = ["moveTabs", "effectWindows", "effectTabGroups"];
-    const [moveTabs, effectWindows, effectTabGroups] =
-        settingFields.map(isPropertyEnabled);
-    chrome.storage.local.set({
-        options: { moveTabs, effectWindows, effectTabGroups },
-    });
+    const [moveTabs, effectWindows, effectTabGroups] = settingFields.map(isOptionEnabled);
+    await updateOptions({ moveTabs, effectWindows, effectTabGroups });
 }
 
-submit.addEventListener("click", () => {
-    submitChanges();
-});
+submit.addEventListener("click", submitChanges);
 
-exclusionBox.addEventListener("keyup", (event) => {
-    if (event.code === "Enter") {
-        if (/\s/.test(exclusionBox.value) || !exclusionBox.value) {
-            return;
-        }
-        chrome.storage.local.get(
-            { exclusionArray: [] },
-            ({ exclusionArray: oldExclusionArray }) => {
-                if (oldExclusionArray.includes(exclusionBox.value)) {
-                    exclusionBox.value = "";
-                    return;
-                }
-                const exclusionArray = [
-                    ...oldExclusionArray,
-                    exclusionBox.value,
-                ];
-                chrome.storage.local.set({ exclusionArray });
-                addToHTMLList(exclusionBox.value);
-            }
-        );
+exclusionBox.addEventListener("keyup", async ({ code }) => {
+    const { value: newExclusion } = exclusionBox;
+    if (
+        code !== 'Enter'
+        || !newExclusion
+        || /\s/.test(newExclusion)
+    ) {
+        return;
     }
+
+    const { options: { exclusions } } = await getLocalStorageKey('options');
+    if (exclusions.includes(newExclusion)) {
+        exclusionBox.value = "";
+        return;
+    }
+
+    const newExclusions = [...exclusions, newExclusion];
+    await updateOptions({ exclusions: newExclusions });
+    addToHTMLList(newExclusion);
 });
